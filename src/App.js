bf1b8dd1b4f199edb5b8a3f555c73c795dbe60a9
OYama <lylian.levoyes@outlook.fr>
2021-04-15 16:01:01 +0200

    V 0.1.0 - Initial commit - the begining of the project Adding the first files

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function PromptHome () {
+
+  return (<>
+  <div className="navbar">
+    
+  </div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1> -- WIP -- </h1>
+    </header>
+  </>)
+}
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <PromptHome/>
+    </div>
+  );
+}
+
+export default App;
