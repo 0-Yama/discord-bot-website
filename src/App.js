1c5d267f4ac69a02e95f7e2758456d04909dd27b
OYama_ <llevoyes@myges.fr>
2021-04-21 11:18:36 +0200

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+<<<<<<< HEAD
 function PromptHome () {
 
   return (<>
@@ -20,6 +21,25 @@ function App() {
   return (
     <div className="App">
         <PromptHome/>
+=======
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> Initialize project using Create React App
     </div>
   );
 }
