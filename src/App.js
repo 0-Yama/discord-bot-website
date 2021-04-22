a807a3a72a2de7c1078db4a13b7c07419357028d
OYama_ <llevoyes@myges.fr>
2021-04-22 16:31:51 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+function PromptHome () {
+
+  return (<>
+  <div className="navbar">
+    
+  </div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1> -- WIP -- </h1>
+    </header>
+  </>)
+}
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <PromptHome/>
     </div>
   );
 }
